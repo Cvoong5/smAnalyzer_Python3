7a443a05b108fa968420829a2e3a6605ed6aa753
Calvin <Voongcal@gmail.com>
2023-12-08 19:22:03 -0700

    Updating smSupport.py and smTestenv.py

--- smColocalize/smSupport.py
+++ smColocalize/smSupport.py
@@ -5,6 +5,7 @@ import tifffile
 import nd2
 import glob
 import matplotlib.pyplot as plt
+from scipy.ndimage import zoom
 
 def nd2_to_tif():
     directory = input("Enter path directory")
@@ -46,8 +47,7 @@ def display_image(image):
         img = image[i]
         axs[i].imshow(img, cmap = 'gray')
     plt.show()
-
-def offset_image(image, angle = 0, y_offset = 0, x_offset = 0, flip = None):
+def image_transform(image, angle = 0, y_offset = 0, x_offset = 0, flip = None):
         #flip = h for horizontal or v for vertical 
         shifted_image = np.zeros_like(image)
         row, col = image.shape
@@ -75,34 +75,8 @@ def offset_image(image, angle = 0, y_offset = 0, x_offset = 0, flip = None):
                         if y_shift >= 0 and y_shift < row and x_shift >= 0 and x_shift < col:
                                 shifted_image[y, x] = image[y_shift, x_shift]
         return shifted_image
-
-def offset_coordinates(image_shape, coordinates, angle, y_offset, x_offset, mode = "list"):
-    row, col = image_shape
-    ycenter = row//2
-    xcenter = col//2
-    theta = np.radians(angle)
-    shifted_coordinates = []
-    if mode == "list":
-        for coordinate in coordinates:
-            y, x = coordinate
-            x_shift = int(((x - xcenter)*np.cos(theta) - (y - ycenter)*np.sin(theta) + xcenter) + x_offset)
-            y_shift = int(((x - xcenter)*np.sin(theta) + (y - ycenter)*np.cos(theta) + ycenter) + y_offset)
-            if y_shift >= 0 and y_shift < row and x_shift >=0 and x_shift < col:
-                shifted_coordinates.append((y_shift, x_shift))
-        return shifted_coordinates 
-
-    elif mode == "dataframe":
-        for i in range(len(coordinates)):
-            x = coordinates['x'][i]
-            y = coordinates['y'][i]
-            x_shift = int(((x - xcenter)*np.cos(theta) - (y - ycenter)*np.sin(theta) + xcenter) + x_offset)
-            y_shift = int(((x - xcenter)*np.sin(theta) + (y - ycenter)*np.cos(theta) + ycenter) + y_offset)
-            if y_shift >= 0 and y_shift < row and x_shift >=0 and x_shift < col:
-                temp = {'x':x_shift, 'y':y_shift}
-                shifted_coordinates.append(temp)
-        shifted_coordinates_df = pd.DataFrame(shifted_coordinates)
-        return shifted_coordinates_df
-    
+def image_zoom(image, x_zoom = 0, y_zoom = 0):
+    return 
 def coordinates_list_to_dataframe(coordinates):
     organized_coordinates = []
     for coordinate in coordinates:
@@ -111,6 +85,7 @@ def coordinates_list_to_dataframe(coordinates):
         organized_coordinates.append(coordinates_separated)
     coordinates_df = pd.DataFrame(organized_coordinates)
     return coordinates_df
+
 
 
 
--- smColocalize/smTestenv.py
+++ smColocalize/smTestenv.py
@@ -6,27 +6,13 @@ import smSupport as smS
 import smCore as smC
 import smColocalize as sm2
 import matplotlib.pyplot as plt
-
+import scipy
 
 #Read files
 
-directory = '/Users/calvin/Desktop/smF_analyzer/analysis'
+directory = '/Users/calvin/Library/CloudStorage/OneDrive-UCB-O365/7 Single_molecule_data/smMovies/Calvin/2023/20231207/Experiment1'
 os.chdir(directory)
-file1 = 'Green002.tif'
-file2 = 'Red002.tif'
+file1 = 'HMGB1001.nd2'
 
-movie = smC.read_movie(file1)
-movie2 = smC.read_movie(file2)
-summed_image = smC.sum_movie(movie)
-summed_image2 = smC.sum_movie(movie2)
-flipped_image = smS.offset_image(summed_image2, angle = 10, x_offset = -5, flip = "h")
-
-sm2.overlay_images(summed_image, flipped_image)
-
-#coordinates1 = smC.find_spots(image1, 3)
-#ocoordinates1 = smS.organize_coordinates(coordinates1)
-#movie2, image2 = smC.read_data(file2)
-#coordinates2 = smC.find_spots(image2, 1)
-#ocoordinates1 = smS.organize_coordinates(coordinates2)
-
+movie2 = smC.read_movie(file1)
 
