fdf71a2d66415fa302371599c8b2dad504082c91
Calvin <voongcal@gmail.com>
2023-12-11 21:58:01 -0700

    working item

--- Test/gauss2d.py
+++ Test/gauss2d.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
+import scipy.optimize as opt
 
 #Function to generate a 2D Gaussian
-def gauss2d(x = 0, y = 0, xo = 0, yo = 0, sx = 1, sy = 1):
+def gauss2d(xy = (512, 512), xo = 0, yo = 0, sx = 1, sy = 1):
     #Set up to generate a single molecule based on 2D Gaussian
+    x, y = xy
     normalization = 1/(2*np.pi*sx*sy)
     x_term = ((x-xo)**2/sx**2)
     y_term = ((y-yo)**2/sy**2)
@@ -20,8 +21,16 @@ def generate_random_molecules(num_molecules = 100, image_size = (512, 512)):
     for _ in range(num_molecules):
         xo = np.random.randint(0, image_size[1])
         yo = np.random.randint(0, image_size[0])
-        image += gauss2d(x, y, xo, yo)
+        image += gauss2d((x,y), xo, yo)
     return image
 
-def local_maxima_2d():
-    pass
+#Needs some work
+def gauss2dfit(data):
+    rows, cols = data.shape
+    empty_data = np.zeros_like(data)
+    x, y = np.meshgrid(np.arange(cols), np.arange(rows))
+    intensity_max = data.argmax()
+    guess = [x.ravel()[intensity_max], y.ravel()[intensity_max], 1, 1]
+    parameters, covariance = opt.curve_fit(gauss2d, empty_data, data, p0 = guess)
+    return 
+
